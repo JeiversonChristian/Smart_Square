33b56448dbffa1c77e11e58ed1e4ba6275f41405
JeiversonChristian <jeiversonc@gmail.com>
2023-05-11 22:57:04 -0300

    criando a função de desenhar a tela e a função principal

--- smart_square.py
+++ smart_square.py
@@ -11,3 +11,20 @@ QUADRADO_HUMANO_IMG = pygame.image.load('imgs/quadrado_humano.jpg')
 MURO = pygame.image.load('imgs/muro.jpg')
 CHAO = pygame.image.load('imgs/chao.jpg')
 
+def desenhar_tela(tela):
+    
+    tela.blit(CHAO, (0,0))
+    pygame.display.update()
+
+def main():
+    #relogio = pygame.time.Clock()
+    tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
+    rodando = True
+    while rodando:
+        #relogio.tick(30)
+        desenhar_tela(tela)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                rodando = False
+
+main()
