72e802495df030c2c876069f6efb86e48a45c530
JeiversonChristian <jeiversonc@gmail.com>
2023-05-11 22:04:32 -0300

    começando a criar o programa

---
+++ smart_square.py
@@ -0,0 +1,13 @@
+# Bibliotecas 
+
+import pygame
+import os
+
+# Constantes
+
+LARGURA_TELA = 500
+ALTURA_TELA = 700
+QUADRADO_HUMANO_IMG = pygame.image.load('imgs/quadrado_humano.jpg')
+MURO = pygame.image.load('imgs/muro.jpg')
+CHAO = pygame.image.load('imgs/chao.jpg')
+
